c100b85aef2da3e8cc48cb495bf3d3265b4eadc7
Michael Robards <mikerobards@icloud.com>
2021-10-08 17:11:54 -0400

    add properties div

--- utils.ts
+++ utils.ts
@@ -1,6 +1,7 @@
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
+const propertiesDisplay = document.querySelector('.properties')
 
 export function displayReviewTotal(value: number, reviewer: string, loyalty: boolean) {
     const iconDisplay = loyalty ? '🌟' : ''
@@ -13,4 +14,8 @@ export function populateUser(isReturning: boolean, userName: string ) {
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
+}
+
+export function displayProperties() {
+    
 }
