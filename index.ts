f44ed52d265c81c44778b5432642defc99cd9df1
Michael Robards <mikerobards@icloud.com>
2021-10-11 15:30:07 -0400

    add enum for loyalty

--- index.ts
+++ index.ts
@@ -1,32 +1,36 @@
-const propertyContainer = document.querySelector('.properties')
-const footer = document.querySelector(.'footer')
-
 import { displayReviewTotal, populateUser } from './utils'
+import { Permissions, LoyaltyUser } from './enums'
+
+const propertyContainer = document.querySelector('.properties')
+const footer = document.querySelector('.footer')
+
+
 let isOpen: boolean
+
 
 // Reviews
 const reviews: {
     name: string;
     stars: number;
-    loyaltyUser: boolean;
+    loyaltyUser: LoyaltyUser;
     date: string;
 }[] = [
     {
         name: 'Sheia',
         stars: 5,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: false,
+        loyaltyUser: LoyaltyUser.BRONZE_USER,
         date: '28-03-2021'
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
     }
 ]
@@ -45,13 +49,6 @@ const reviews: {
 //     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 // }
 
-const ADMIN = 'admin'
-const READ_ONLY = 'read-only'
-
-enum Permissions {
-    ADMIN,
-    READ_ONLY 
-}
 
 const you = {
     userName: {firstName: 'Mike', lastName: 'Brown'},
--- utils.ts
+++ utils.ts
@@ -1,10 +1,12 @@
+import { Permissions, LoyaltyUser } from "./enums"
+
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 
-export function displayReviewTotal(value: number, reviewer: string, loyalty: boolean) {
-    const iconDisplay = loyalty ? '🌟' : ''
+export function displayReviewTotal(value: number, reviewer: string, loyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '🌟' : ''
     reviewTotalDisplay.innerHTML = 
         `review total ${value.toString()} | last reviewed by ${reviewer} ${iconDisplay}`
 }
