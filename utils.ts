eefe430f2336034503eb00e279fc635703a5bede
Michael Robards <mikerobards@icloud.com>
2021-10-12 16:47:38 -0400

    add classes file

--- utils.ts
+++ utils.ts
@@ -2,7 +2,7 @@ const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 import { LoyaltyUser, Permissions } from './enums'
-import  Review  from './interfaces'
+import  { Review }  from './interfaces'
 
 export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
     const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
