3ae8f9251e315208c716215f7ecf606e36aaf753
Michael Robards <mikerobards@icloud.com>
2021-10-12 16:59:26 -0400

    complie

---
+++ classes.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.MainProperty = void 0;
+var MainProperty = /** @class */ (function () {
+    function MainProperty(src, title, reviews) {
+        this.src = src;
+        this.title = title;
+        this.reviews = reviews;
+    }
+    return MainProperty;
+}());
+exports.MainProperty = MainProperty;
