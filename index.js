53ed994d56bcd2acfa7c07ccc7b1607240e52df9
Michael Robards <mikerobards@icloud.com>
2021-10-12 17:13:13 -0400

    work error

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
-export const __esModule = true;
-import { showReviewTotal, populateUser, showDetails, getTopTwoReviews } from "./utils";
-import { LoyaltyUser, Permissions } from "./enums";
-import { MainProperty } from "./classes";
+exports.__esModule = true;
+var utils_1 = require("./utils");
+var enums_1 = require("./enums");
+var classes_1 = require("./classes");
 var propertyContainer = document.querySelector('.properties');
 var reviewContainer = document.querySelector('.reviews');
 var container = document.querySelector('.container');
@@ -14,26 +14,26 @@ var reviews = [
     {
         name: 'Sheila',
         stars: 5,
-        loyaltyUser: LoyaltyUser.GOLD_USER,
+        loyaltyUser: enums_1.LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: LoyaltyUser.BRONZE_USER,
+        loyaltyUser: enums_1.LoyaltyUser.BRONZE_USER,
         date: '28-03-2021'
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: LoyaltyUser.SILVER_USER,
+        loyaltyUser: enums_1.LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
     },
 ];
 var you = {
     firstName: 'Bobby',
     lastName: 'Brown',
-    permissions: Permissions.ADMIN,
+    permissions: enums_1.Permissions.ADMIN,
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
@@ -94,8 +94,8 @@ var properties = [
     }
 ];
 // Functions
-(0, showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
-(0, populateUser)(you.isReturning, you.firstName);
+(0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+(0, utils_1.populateUser)(you.isReturning, you.firstName);
 // Add the properties
 for (var i = 0; i < properties.length; i++) {
     var card = document.createElement('div');
@@ -104,14 +104,14 @@ for (var i = 0; i < properties.length; i++) {
     var image_1 = document.createElement('img');
     image_1.setAttribute('src', properties[i].image);
     card.appendChild(image_1);
-    (0, showDetails)(you.permissions, card, properties[i].price);
+    (0, utils_1.showDetails)(you.permissions, card, properties[i].price);
     propertyContainer.appendChild(card);
 }
 var count = 0;
 function addReviews(array) {
     if (!count) {
         count++;
-        var topTwo = (0, getTopTwoReviews)(array);
+        var topTwo = (0, utils_1.getTopTwoReviews)(array);
         for (var i = 0; i < topTwo.length; i++) {
             var card = document.createElement('div');
             card.classList.add('review-card');
@@ -124,10 +124,10 @@ function addReviews(array) {
 button.addEventListener('click', function () { return addReviews(reviews); });
 var currentLocation = ['London', '11.03', 17];
 footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°';
-var yourMainProperty = new MainProperty('images/italian-property.jpg', 'Italian House', [{
+var yourMainProperty = new classes_1.MainProperty('images/italian-property.jpg', 'Italian House', [{
     name: 'Olive',
     stars: 5,
-    loyaltyUser: LoyaltyUser.GOLD_USER,
+    loyaltyUser: enums_1.LoyaltyUser.GOLD_USER,
     date: '12-04-2021'
 }]);
 var mainImageContainer = document.querySelector('.main-image');
