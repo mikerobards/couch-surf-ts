3a09c8ff8d7c846a7ac6c6ded243a2c125fbcde3
Michael Robards <mikerobards@icloud.com>
2021-10-12 15:52:35 -0400

    update files

--- index.ts
+++ index.ts
@@ -1,34 +1,44 @@
-<<<<<<< HEAD
-import { displayReviewTotal, populateUser } from './utils'
-import { Permissions, LoyaltyUser } from './enums'
+// Wrapping up our Dashboard
+// Style the 'Get Reviews' Button
+// Make the button green
+// Make the text white
+// Give the button a 5px border radius
+// Make the button turn yellow on hover
 
+import { showReviewTotal, populateUser, showDetails, getTopTwoReviews} from './utils'
+import { Price, Country } from './types'
+import { Permissions , LoyaltyUser } from './enums'
+import  Review  from './interfaces'
 const propertyContainer = document.querySelector('.properties')
+const reviewContainer = document.querySelector('.reviews')
+const container = document.querySelector('.container')
+const button = document.querySelector('button')
 const footer = document.querySelector('.footer')
 
-
-=======
-import { displayReviewTotal, populateUser, displayProperties } from './utils'
->>>>>>> f97279fd34595ba5b4595539bd795e075a5c4c71
-let isOpen: boolean
 let isLoggedIn: boolean
 
+enum Permissions {
+    ADMIN = 'ADMIN', 
+    READ_ONLY = 'READ_ONLY'
+}
+
+enum LoyaltyUser {
+    GOLD_USER = 'GOLD_USER',
+    SILVER_USER = 'SILVER_USER',
+    BRONZE_USER = 'BRONZE_USER'
+}
+
+interface Review {
+    name: string; 
+    stars: number; 
+    loyaltyUser: LoyaltyUser; 
+    date: string;   
+}
 
 // Reviews
-const reviews: ({
-    name: string;
-    stars: number;
-    loyaltyUser: LoyaltyUser;
-    date: string;
-} |
-{
-    name: string;
-    stars: number;
-    loyaltyUser: LoyaltyUser;
-    date: string;
-    description: string;
-})[] = [
+const reviews: Review[] = [
     {
-        name: 'Sheia',
+        name: 'Sheila',
         stars: 5,
         loyaltyUser: LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
@@ -44,126 +54,148 @@ const reviews: ({
         stars: 4,
         loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021',
-        description: 'AWESOME COUCH!!!!!!'
-    }
+    },
 ]
 
-
-// user
-// const you: {
-//     userName: {firstName : string, lastName : string};
-//     isReturning: boolean;
-//     age: number;
-//     stayedAt: string[];
-// } = {
-//     userName: {firstName: 'Mike', lastName: 'Brown'},
-//     isReturning: true,
-//     age: 50,
-//     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-// }
-
-
 const you = {
-    userName: {firstName: 'Mike', lastName: 'Brown'},
-    permissions: Permissions.ADMIN, 
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    permissions: Permissions.ADMIN,
     isReturning: true,
-    age: 50,
+    age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-
-
-
-
-const properties: {
-        img: string;
-        title: string;
-        price: number;
-        location: {
-            streetAddress: string;
-            city: string;
-            state: string;
-            zipcode: number;
-            country: string;
-        };
-        contactDetails: [number, string];
-        isAvailableToRent: boolean;
-}[] = [
-    {
-        img: './images/todd-kent-178j8tJrNlc-unsplash.jpg',
-        title: 'Lovely Country Home!',
-        price: 500,
-        location: {
-            streetAddress: '123 Main Street',
-            city: 'Niceville',
-            state: 'Florida',
-            zipcode: 12345,
-            country: 'USA'
-        },
-        contactDetails: [8506521687, 'landlord@home.com'],
-        isAvailableToRent: true
-    },
-    {
-        img: './images/ralph-ravi-kayden-2d4lAQAlbDA-unsplash.jpg',
-        title: 'Super Posh Bungaloo!',
-        price: 1500,
-        location: {
-            streetAddress: '123 Beach Drive',
-            city: 'South Beach',
-            state: 'Florida',
-            zipcode: 65432,
-            country: 'USA'
-        },
-        contactDetails: [7706548451, 'madoff@aol.com'],
-        isAvailableToRent: true
-    },
-    {
-        img: './images/sieuwert-otterloo-aren8nutd1Q-unsplash.jpg',
-        title: 'Charming Family Dwelling!',
-        price: 7500,
-        location: {
-            streetAddress: '123 Country Avenue',
-            city: 'Youngstown',
-            state: 'Alabama',
-            zipcode: 98745,
-            country: 'USA'
-        },
-        contactDetails: [6652125481, 'bobbyjoe@yahoo.com'],
-        isAvailableToRent: false
+interface Property {
+    image: string;
+    title: string;
+    price: Price;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number | string;
+        country: Country
     }
+    contact: [ number, string];
+    isAvailable: boolean;
+}
 
+// Array of Properties
+const properties : Property[] = [
+    {
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: [+112343823978921, 'marywinkle@gmail.com'],
+        isAvailable: true  
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 30,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: [+1298239028490830, 'garydavis@hotmail.com'],
+        isAvailable: false 
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 25,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 'SW4 5XW',
+            country: 'United Kingdom',
+        },
+        contact: [+34829374892553, 'andyluger@aol.com'],
+        isAvailable: true
+    },
+    {
+        image: 'images/malaysian-hotel.jpeg',
+        title: 'Malia Hotel',
+        price: 35,
+        location: {
+            firstLine: 'Room 4',
+            city: 'Malia',
+            code: 45334,
+            country: 'Malaysia'
+        },
+        contact: [ +60349822083, 'lee34@gmail.com'],
+        isAvailable: false
+    }
 ]
 
-displayReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-populateUser(you.isReturning, you.userName.firstName)
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
-<<<<<<< HEAD
-let authStatus : any
+populateUser(you.isReturning, you.firstName)
 
-isLoggedIn = true;
-
-function showDetails(authorityStatus: any, element : HTMLDivElement, price: number) {
-    if (authorityStatus) {
-        const priceDisplay = document.createElement('div')
-        priceDisplay.innerHTML = price.toString() + '/night'
-        element.appendChild(priceDisplay)
-    }
- }
-
-for (let i=0; i < properties.length; i++) {
+// Add the properties
+for (let i = 0; i < properties.length; i++) {
     const card = document.createElement('div')
     card.classList.add('card')
     card.innerHTML = properties[i].title
     const image = document.createElement('img')
-    image.setAttribute('src', properties[i].img)
+    image.setAttribute('src', properties[i].image)
     card.appendChild(image)
+    showDetails(you.permissions, card, properties[i].price)
     propertyContainer.appendChild(card)
-    showDetails(authStatus, card, properties[i].price)
 }
 
-//use your location, your current time, and the current temp
-let currentLocation : [string, string, number] = ['Kennesaw, GA', '2:23PM', 75]
-footer.innerHTML = `${currentLocation[0]} ${currentLocation[1]} ${currentLocation[2]}°F`
-=======
-displayProperties(properties);
->>>>>>> f97279fd34595ba5b4595539bd795e075a5c4c71
+let count = 0
+function addReviews(array : Review[]) : void {
+    if (!count ) {
+        count++
+        const topTwo = getTopTwoReviews(array)
+        for (let i = 0; i < topTwo.length; i++) {
+            const card = document.createElement('div')
+            card.classList.add('review-card')
+            card.innerHTML = topTwo[i].stars + ' stars from ' + topTwo[i].name
+            reviewContainer.appendChild(card)
+        }
+        container.removeChild(button) 
+    }
+}
+
+button.addEventListener('click', () => addReviews(reviews))
+
+let currentLocation : [string, string, number] = ['London', '11.03', 17]
+footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
+
+// Classes
+class MainProperty {
+    src: string
+    title: string
+    reviews: Review[]
+    constructor(src: string, title: string, reviews: Review[]) {
+        this.src = src
+        this.title = title
+        this.reviews = reviews
+    }
+}
+
+let yourMainProperty = new MainProperty(
+    'images/italian-property.jpg', 
+    'Italian House',
+    [{
+        name: 'Olive',
+        stars: 5,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
+        date: '12-04-2021'
+    }] )
+
+const mainImageContainer = document.querySelector('.main-image')
+const image = document.createElement('img')
+image.setAttribute('src', yourMainProperty.src)
+mainImageContainer.appendChild(image)
